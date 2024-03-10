cde27bf1e1fb35b419f06645dc4e896cd9951bde
ancientElement <642211794@qq.com>
2024-03-10 13:43:59 +0800

    主体框架搭建完毕

---
+++ AETinyrenderer/Src/GL.cpp
@@ -0,0 +1,6 @@
+﻿#include <Eigen/Dense>
+#include <iostream>
+#include <vector>
+#include "GL.h"
+#include "tgaimage.h"
+using namespace Eigen;
---
+++ AETinyrenderer/Src/GL.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "tgaimage.h"
+
+class GL
+{
+public:
+};
